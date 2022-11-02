d170cbde79210b14dd28f4880bcc077aa2e4e200
rz <muhamaul@gmail.com>
2022-11-02 20:54:01 +0700

    [closakata] changed guess limit to n+1 and moved some code to helper

--- src/closakata/core.clj
+++ src/closakata/core.clj
@@ -33,47 +33,6 @@
   "Pick a random word from qualified words"
   []
   (rand-nth all-words))
-
-(defn print-with-color
-  "Print characters with color"
-  [c]
-  (cond
-    (:just c) (print (format "%s%s%s" (:green h/console-fg-color) (:char c) (:reset h/console-fg-color)))
-    (:almost c) (print (format "%s%s%s" (:yellow h/console-fg-color) (:char c) (:reset h/console-fg-color)))
-    (:used c) (print (format "%s%s%s" (:grey h/console-fg-color) (:char c) (:reset h/console-fg-color)))
-    :else (print (format "%s" (:char c)))))
-
-(defn print-keyboard
-  "Print valid keys which formatted by order"
-  [valid-keys]
-  (let [print-seq
-        (fn [seq]
-          (dorun (for [c seq]
-                   (print-with-color
-                    (first (filter #(= (:char %) c) valid-keys))))))]
-    (print-seq '[\Q \W \E \R \T \Y \U \I \O \P])
-    (println)
-    (print " ")
-    (print-seq '[\A \S \D \F \G \H \J \K \L])
-    (println)
-    (print "  ")
-    (print-seq '[\Z \X \C \V \B \N \M])
-    (println)))
-
-(defn print-history
-  "print history with color"
-  [history]
-  (let [print-seq
-        (fn [seq]
-          (dorun (for [c seq]
-                   (print-with-color c))))]
-    (dorun
-     (map #(do
-             (print-seq %)
-             (println))
-          history))))
-
-
 (defn check-guess
   "This function validates and checks the user's guess."
   [line]
@@ -88,28 +47,20 @@
     (do
       (swap! history conj (h/score line @secret)) ; compare guess to secret and put guess in history
       (reset! valid-keys (h/available-keys @history)) ; compute un-guessed characters based on the history
-      (print-keyboard @valid-keys)
+      (h/print-keyboard @valid-keys)
       (println)
-      (print-history @history)
+      (h/print-history @history)
       (if (= line @secret) ; if guess = secret
         (do
           (reset! win true)
           (println "Kamu menang!!!!"))))))
 
-(defn play-again?
-  "This function prompts the user for a new session
-  return true as long as the user's answer is either y or Y"
-  []
-  (print "Main lagi? y/n: ")
-  (flush)
-  (let [respon (read-line)]
-    (or (= respon "y") (= respon "Y"))))
 
 
 (defn has-chance?
   "This function checks if the user still has chance to guess"
   []
-  (and (< (count @history) (dec (count @secret))) (not @win)))
+  (and (< (count @history) (inc (count @secret))) (not @win)))
 
 (defn game
   "game's backbone"
@@ -120,7 +71,7 @@
   (def secret-length (count @secret))
 
   (println "Panjang karakter kata rahasia:" secret-length)
-  (println (str "Kamu punya " (dec secret-length) " kesempatan untuk menebak."))
+  (println (str "Kamu punya " (inc secret-length) " kesempatan untuk menebak."))
 
   (while (has-chance?)
     (println "###############################################")
@@ -131,7 +82,7 @@
   (when (not @win) (println "Kamu kalah!!!"))
   (println "Jawabannya adalah" @secret)
 
-  (when (play-again?)
+  (when (h/play-again?)
     (reset! win false) ; if the user want to play again, set win to false
     (recur)))
 
@@ -140,7 +91,7 @@
   [& args]
   (println "--- Selamat datang di Closakata ---")
   (println (str "Aku akan memilih kata dalam Bahasa Indonesia yang panjangnya " min-length " sampai " max-length " karakter."))
-  (println "Kamu punya n-1 kesempatan untuk menebak, yang mana n adalah panjang kata rahasia.")
+  (println "Kamu punya n+1 kesempatan untuk menebak, yang mana n adalah panjang kata rahasia.")
   (println "Tebak kata rahasia yang aku punya sekarang!")
   (println)
   (println "Keterangan:\nBenar=Hijau Hampir=Kuning Terpakai=Abu-abu")
--- src/closakata/helper.clj
+++ src/closakata/helper.clj
@@ -29,6 +29,55 @@
    :reset   "\u001b[49m"
    })
 
+(defn print-with-color
+  "Print characters with color"
+  [c]
+  (cond
+    (:just c) (print (format "%s%s%s" (:green console-fg-color) (:char c) (:reset console-fg-color)))
+    (:almost c) (print (format "%s%s%s" (:yellow console-fg-color) (:char c) (:reset console-fg-color)))
+    (:used c) (print (format "%s%s%s" (:grey console-fg-color) (:char c) (:reset console-fg-color)))
+    :else (print (format "%s" (:char c)))))
+
+(defn print-keyboard
+  "Print valid keys which formatted by order"
+  [valid-keys]
+  (let [print-seq
+        (fn [seq]
+          (dorun (for [c seq]
+                   (print-with-color
+                    (first (filter #(= (:char %) c) valid-keys))))))]
+    (print-seq '[\Q \W \E \R \T \Y \U \I \O \P])
+    (println)
+    (print " ")
+    (print-seq '[\A \S \D \F \G \H \J \K \L])
+    (println)
+    (print "  ")
+    (print-seq '[\Z \X \C \V \B \N \M])
+    (println)))
+
+(defn print-history
+  "print history with color"
+  [history]
+  (let [print-seq
+        (fn [seq]
+          (dorun (for [c seq]
+                   (print-with-color c))))]
+    (dorun
+     (map #(do
+             (print-seq %)
+             (println))
+          history))))
+
+
+(defn play-again?
+  "This function prompts the user for a new session
+  return true as long as the user's answer is either y or Y"
+  []
+  (print "Main lagi? y/n: ")
+  (flush)
+  (let [respon (read-line)]
+    (or (= respon "y") (= respon "Y"))))
+
 (defn score
   "This function compare guess and secret"
   [guess secret]
